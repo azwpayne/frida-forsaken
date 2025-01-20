528c0556d5ad005e813cc87b9960c75ff0db27be
azwpayne <paynewu0719@gmail.com>
2025-01-20 10:15:38 +0800

    Fix compatibility issues with different android versions

--- src/androidSystemLibrary/libdl/dlope.ts
+++ src/androidSystemLibrary/libdl/dlope.ts
@@ -1,59 +1,31 @@
 'use strict';
 
+let library_loaded = false;
+
 /**
  *
  * @param module_filter_name
  * @param fn
  */
-export function hook_all_dlopen(module_filter_name: string = '', fn: Function) {
-  Java.androidVersion < '8.0.0'
-    ? hook_dlopen(module_filter_name, fn)
-    : hook_android_dlopen_ext(module_filter_name, fn);
-}
+export function hook_dlopen(module_filter_name: string = '', fn: Function) {
 
-export function hook_dlopen(module_filter_name: string, fn: Function): void {
-  const dlopen = Module.getExportByName(null, 'dlopen');
-  console.log('dlopen: ', dlopen);
-  Interceptor.attach(dlopen, {
-    onEnter: function (args) {
-      const path_ptr = args[0];
-      if (path_ptr) {
-        this.path = path_ptr.readCString();
-        if (this.path.indexOf(module_filter_name) >= 0) {
-          this.canhook = true;
-          console.log('dlopen path:', this.path);
-        }
+  const dlopen_addr = parseInt(Java.androidVersion) < 8
+    ? Module.getExportByName(null, 'dlopen')
+    : Module.getExportByName(null, 'android_dlopen_ext');
+
+  Interceptor.attach(dlopen_addr, {
+    onEnter(args) {
+      const library_path = args[0].readCString();
+      if (library_path && library_path.includes(module_filter_name)) {
+        console.log('[...] Loading library : ' + library_path);
       }
+      library_loaded = true;
     },
-
-    onLeave: function (retval) {
-      if (this.canhook) {
+    onLeave(retval) {
+      if (library_loaded) {
+        library_loaded = false;
         return fn();
       }
     },
   });
-
-}
-
-export function hook_android_dlopen_ext(module_filter_name: string, fn: Function): void {
-  const android_dlopen_ext = Module.getExportByName(null, 'android_dlopen_ext');
-  console.log(`android_dlopen_ext: ${android_dlopen_ext}`);
-
-  Interceptor.attach(android_dlopen_ext, {
-    onEnter: function (args) {
-      const path_ptr = args[0];
-      this.path = path_ptr.readCString();
-      if (this.path.includes(module_filter_name)) {
-        this.canhook = true;
-        console.log(`android_dlopen_ext path: ${this.path}`);
-      }
-
-    },
-    onLeave: function (retval) {
-      if (this.canhook) {
-        return fn();
-      }
-    },
-  });
-
 }
